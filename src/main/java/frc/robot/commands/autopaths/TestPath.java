32f007fb5d7d6a2d20b81254cb304031eed4c862
ice-blokk <anthonythomastan@gmail.com>
2021-09-23 16:36:48 -0400

    Cleaned TestPath

--- src/main/java/frc/robot/commands/autopaths/TestPath.java
+++ src/main/java/frc/robot/commands/autopaths/TestPath.java
@@ -6,29 +6,19 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import frc.robot.subsystems.Drivetrain;
 
 public final class TestPath {
-    public static Trajectory getTraj(TrajectoryConfig config) {
+    public static Trajectory getTraj(Drivetrain drivetrain) {
         Trajectory testPath = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(),
+            drivetrain.getCurrentPose(),
             List.of(
-              new Translation2d(1.2, 1),
-              //new Translation2d(1.5,1.5),
-              new Translation2d(2, 1.3),
-              new Translation2d(5.5, 1.1), // this and next waypoint go straight
-              new Translation2d(5.8, -0.1),
-            //new Translation2d(6.2, -.3),
-              new Translation2d(7.5, -.4), 
-              new Translation2d(7.2, 1),
-              new Translation2d(6.2, 1.1),
-              new Translation2d(5.7, 0), // this and next waypoint go straight
-              new Translation2d(2.2, 0),
-              new Translation2d(1.6, .8)
+              new Translation2d(2, 0)
+
             ),
-            new Pose2d(0, 1.6, new Rotation2d(Math.toRadians(180))),
-            config
+            new Pose2d(4.3, 0, new Rotation2d()),
+            drivetrain.getSlowConfig()
           );
 
         return testPath;
