956bcbfddfec291c28846a5534a4b9ab7a4d8edd
ice-blokk <anthonythomastan@gmail.com>
2021-10-02 16:40:59 -0400

    Added pixy to RobotContainer.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.Limelight;
+import frc.robot.util.Pixy2;
+import frc.robot.util.Pixy2Obj;
 
 public class Robot extends TimedRobot {
 
@@ -12,12 +14,14 @@ public class Robot extends TimedRobot {
 	private RobotContainer robotContainer;
 	Joystick stick = new Joystick(0);
 	Limelight limelight;
+	Pixy2Obj pixy;
 
 
 	@Override
 	public void robotInit() {
 		robotContainer = new RobotContainer();
 		limelight = new Limelight();
+		pixy = new Pixy2Obj();
 	}
 
 	@Override
@@ -25,6 +29,14 @@ public class Robot extends TimedRobot {
 		CommandScheduler.getInstance().run();
 		SmartDashboard.putNumber("Slider val", stick.getRawAxis(3));
 		SmartDashboard.putNumber("Limelight Shoot Speed", .18 * limelight.getArea() - .88);
+
+		pixy.updateValues();
+		SmartDashboard.putNumber("Pixy x", pixy.getPixyX());
+		SmartDashboard.putNumber("Pixy y", pixy.getPixyY());
+		SmartDashboard.putNumber("Pixy sig", pixy.getSig());
+		SmartDashboard.putNumber("Pixy height", pixy.getHeight());
+		SmartDashboard.putNumber("Pixy width", pixy.getWidth());
+		SmartDashboard.putNumber("Pixy area", pixy.getPixyArea());
 	}
 	
 	@Override
